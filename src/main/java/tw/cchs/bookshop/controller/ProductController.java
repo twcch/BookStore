040b5a2a75f123dc2ff6c5544c93f0a443ddb6de
謝志謙 <twcch1218@gmail.com>
2024-05-25 20:55:04 +0800

    商品功能 - 查詢商品列表 (優化)

--- src/main/java/tw/cchs/bookshop/controller/ProductController.java
+++ src/main/java/tw/cchs/bookshop/controller/ProductController.java
@@ -1,9 +1,12 @@
 package tw.cchs.bookshop.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import tw.cchs.bookshop.constant.ProductCategory;
 import tw.cchs.bookshop.dto.ProductQueryParams;
@@ -14,6 +17,7 @@ import tw.cchs.bookshop.service.ProductService;
 import java.util.List;
 
 @RestController
+@Validated
 public class ProductController {
 
     @Autowired
@@ -37,13 +41,19 @@ public class ProductController {
             @RequestParam(required = false) String search,
             // 排序 Sorting
             @RequestParam(defaultValue = "created_date") String orderBy,
-            @RequestParam(defaultValue = "DESC") String sort) {
+            @RequestParam(defaultValue = "DESC") String sort,
+            // 分頁 Pagination
+            @RequestParam(defaultValue = "10") @Max(1000) @Min(0) Integer limit,
+            @RequestParam(defaultValue = "0") @Min(0) Integer offset
+    ) {
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList = productService.getProducts(productQueryParams);
 
