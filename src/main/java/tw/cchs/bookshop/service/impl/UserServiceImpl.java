b171f6552feb3ac66db40faab7200409bd5141c1
謝志謙 <twcch1218@gmail.com>
2024-05-26 21:52:28 +0800

    用戶功能 - 登入帳號 (優化)

--- src/main/java/tw/cchs/bookshop/service/impl/UserServiceImpl.java
+++ src/main/java/tw/cchs/bookshop/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 import tw.cchs.bookshop.dao.UserDao;
 import tw.cchs.bookshop.dto.UserLoginRequest;
@@ -31,6 +32,10 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
+
         // 創建帳號
         return userDao.createUser(userRegisterRequest);
 
@@ -47,8 +52,11 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        // 使用 MD5 生成密碼的雜湊值
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
         // 檢查密碼
-        if (!user.getPassword().equals(userLoginRequest.getPassword())) {
+        if (!user.getPassword().equals(hashedPassword)) {
             log.warn("該 email {} 的密碼不正確", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
