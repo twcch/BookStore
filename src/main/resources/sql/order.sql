87ae731b31d03fae07ce58e1405af8a1678b0465
謝志謙 <twcch1218@gmail.com>
2024-05-31 19:24:49 +0800

    新增備份 sql 的檔案

--- src/main/resources/sql/order.sql
+++ src/main/resources/sql/order.sql
@@ -1,12 +1,13 @@
 create table `order`
 (
-    order_id           int auto_increment
-        primary key,
+    order_id           int auto_increment primary key,
     user_id            int       not null,
     total_amount       int       not null,
     created_date       timestamp not null,
     last_modified_date timestamp not null
 );
 
-INSERT INTO mysql_bookshop.`order` (order_id, user_id, total_amount, created_date, last_modified_date) VALUES (1, 6, 100110, '2022-06-02 16:51:49', '2022-06-02 16:51:49');
-INSERT INTO mysql_bookshop.`order` (order_id, user_id, total_amount, created_date, last_modified_date) VALUES (4, 1, 2997, '2024-05-29 20:23:09', '2024-05-29 20:23:09');
+INSERT INTO `order` (user_id, total_amount, created_date, last_modified_date)
+VALUES (6, 100110, '2022-06-02 16:51:49', '2022-06-02 16:51:49');
+INSERT INTO `order` (user_id, total_amount, created_date, last_modified_date)
+VALUES (1, 2997, '2024-05-29 20:23:09', '2024-05-29 20:23:09');
--- src/main/resources/sql/order_item.sql
+++ src/main/resources/sql/order_item.sql
@@ -1,14 +1,13 @@
 create table order_item
 (
-    order_item_id int auto_increment
-        primary key,
+    order_item_id int auto_increment primary key,
     order_id      int not null,
     product_id    int not null,
     quantity      int not null,
     amount        int not null
 );
 
-INSERT INTO mysql_bookshop.order_item (order_item_id, order_id, product_id, quantity, amount) VALUES (1, 1, 4, 2, 60);
-INSERT INTO mysql_bookshop.order_item (order_item_id, order_id, product_id, quantity, amount) VALUES (2, 1, 6, 5, 50);
-INSERT INTO mysql_bookshop.order_item (order_item_id, order_id, product_id, quantity, amount) VALUES (3, 1, 7, 1, 100000);
-INSERT INTO mysql_bookshop.order_item (order_item_id, order_id, product_id, quantity, amount) VALUES (4, 4, 1, 3, 2997);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 4, 2, 60);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 6, 5, 50);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (1, 7, 1, 100000);
+INSERT INTO order_item (order_id, product_id, quantity, amount) VALUES (4, 1, 3, 2997);
